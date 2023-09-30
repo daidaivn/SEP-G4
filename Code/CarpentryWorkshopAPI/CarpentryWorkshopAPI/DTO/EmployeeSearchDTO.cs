c8c52a5ebee7c5770cb4aa0c985207fafaca72ab
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-09-30 22:26:52 +0700

    Update create employee and update, search

--- Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/DTO/EmployeeSearchDTO.cs
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/DTO/EmployeeSearchDTO.cs
@@ -2,18 +2,11 @@
 {
     public class EmployeeSearchDTO
     {
-        public int EmployeeId { get; set; }
-        public string? Image { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Dob { get; set; }
-        public string? Gender { get; set; }
-        public string? Address { get; set; }
+       
+        public string? Name { get; set; }
+        public bool? Gender { get; set; }
         public string? PhoneNumber { get; set; }
-        public int? TaxId { get; set; }
-        public string? Email { get; set; }
-        public string? Country { get; set; }
         public bool? Status { get; set; }
-        public string? Cic { get; set; }
+        public List<int>? RoleID { get; set; }
     }
 }
---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/DTO/SearchRoleStatusHistoryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CarpentryWorkshopAPI.DTO
+{
+    public class SearchRoleStatusHistoryDTO
+    {
+        public int RoleId { get; set; }
+        public DateTime? ActionDate { get; set; }
+    }
+}
