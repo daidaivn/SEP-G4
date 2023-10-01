bc30011fbe7cdc8d63c72061c1b6528e9cc02a44
unknown <donghiem114@gmail.com>
2023-10-01 20:57:33 +0700

    fix css list employy

--- Code/CarpentryWorkshopClient/src/App.js
+++ Code/CarpentryWorkshopClient/src/App.js
@@ -4,19 +4,18 @@ import MenuComponent from "./view/component/MenuComponent";
 import ListEmployeeComponent from "./view/component/ListEmployeeComponent";
 import DashboardComponent from "./view/component/DashboardComponnet";
 import NotFoundComponent from "./view/component/NotFoundComponent";
-
-
 import { Col, Row } from "antd";
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="layout-menu">
-
+      <Container fluid>
       <Row>
-        <Col span={4}>
+        <Col xxl={4} span={4}>
           <MenuComponent />
         </Col>
-        <Col span={20}>
+        <Col xxl={20} span={20}>
           <Routes>
             <Route path="/" element={<DashboardComponent />} />
             <Route path="/list-employee" element={<ListEmployeeComponent />} />
@@ -24,6 +23,7 @@ function App() {
           </Routes>
         </Col>
       </Row>
+      </Container>
     </div>
   );
 }
