09ecec0121f7366de53071c53b3ab3064ab4234c
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-10-17 00:53:10 +0700

    Update model and database

---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Models/ShiftType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarpentryWorkshopAPI.Models
+{
+    public partial class ShiftType
+    {
+        public ShiftType()
+        {
+            HistoryChangeShiftTypes = new HashSet<HistoryChangeShiftType>();
+            WorkSchedules = new HashSet<WorkSchedule>();
+        }
+
+        public int ShiftTypeId { get; set; }
+        public string? TypeName { get; set; }
+        public bool? Status { get; set; }
+        public double? HourWork { get; set; }
+
+        public virtual ICollection<HistoryChangeShiftType> HistoryChangeShiftTypes { get; set; }
+        public virtual ICollection<WorkSchedule> WorkSchedules { get; set; }
+    }
+}
---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Models/WorkScheduleStatusHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarpentryWorkshopAPI.Models
+{
+    public partial class WorkScheduleStatusHistory
+    {
+        public int HistoryId { get; set; }
+        public int? WorkScheduleId { get; set; }
+        public string? Action { get; set; }
+        public DateTime? ActionDate { get; set; }
+        public int? CurrentEmployeeId { get; set; }
+
+        public virtual WorkSchedule? WorkSchedule { get; set; }
+    }
+}
