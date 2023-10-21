aef3a46e05f179e9a510e8958d2ac76940bfaa95
unknown <donghiem114@gmail.com>
2023-10-21 10:49:55 +0700

    login auther FE

---
+++ Code/CarpentryWorkshopClient/src/Auther/actions.js
@@ -0,0 +1,4 @@
+export const updatePermissions = (permissions) => ({
+    type: 'UPDATE_PERMISSIONS',
+    payload: permissions,
+  });
--- Code/CarpentryWorkshopClient/src/view/component/componentUI/ListUserHeader.js
+++ Code/CarpentryWorkshopClient/src/view/component/componentUI/ListUserHeader.js
@@ -1,11 +1,20 @@
+import React, { useEffect, useState } from 'react';
 import user from "../../assets/images/Ellipse 69.svg";
 import notification from "../../assets/images/icons/notification.svg";
 import "../../scss/index.scss";
 
 function ListUserHeader() {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    const storedUserName = localStorage.getItem('userName') || sessionStorage.getItem('userName');
+    if (storedUserName) {
+      setUserName(storedUserName);
+    }
+  }, []);
   return (
     <div className="list-user-header">
-      <span>User</span>
+      <span>{userName ? `${userName}` : 'User'}</span>
       <img className="user-list" src={user} alt="" />
       <img className="notification-list" src={notification} alt="" />
     </div>
