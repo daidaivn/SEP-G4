138f2374eefcad8a106936687d66e33b9c5cd2e4
unknown <donghiem114@gmail.com>
2023-10-26 23:21:37 +0700

    new component employee

---
+++ Code/CarpentryWorkshopClient/src/view/component/componentEmployee/index.js
@@ -0,0 +1,4 @@
+export { default as TableEmployee } from "./TableEmployee.js";
+export { default as TableEmployeeRes } from "./TableEmployeeRes.js";
+export { default as AddEmployeeModal} from "./AddEmployeeModal.js";
+export { default as DetailEmployeeModal} from "./DetailEmployeeModal.js";
