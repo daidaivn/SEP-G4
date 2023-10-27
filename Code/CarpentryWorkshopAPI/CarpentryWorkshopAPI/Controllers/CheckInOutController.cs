e2dc0beb8cbf4a2a45c573decbb28f4558228a39
HoangTran21012001 <hoangtran2k1000@gmail.com>
2023-10-27 21:00:57 +0700

    j

--- Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Controllers/CheckInOutController.cs
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Controllers/CheckInOutController.cs
@@ -10,6 +10,7 @@ namespace CarpentryWorkshopAPI.Controllers
     [ApiController]
     public class CheckInOutController : ControllerBase
     {
+
         private readonly SEPG4CCMSContext _context;
         private IMapper _mapper;
         public CheckInOutController(SEPG4CCMSContext context, IMapper mapper)
@@ -17,9 +18,8 @@ namespace CarpentryWorkshopAPI.Controllers
             _context = context;
             _mapper = mapper;
         }
-        [HttpGet]
-        [Route("api/attendance/{employeeId}")]
-        public IActionResult GetAttendanceStatus(int employeeId)
+
+        public string GetAttendanceStatus(int employeeId)
         {
             DateTime currentDate = DateTime.Now.Date;
 
@@ -37,10 +37,10 @@ namespace CarpentryWorkshopAPI.Controllers
             {
                 if (DateTime.Now.TimeOfDay > team.Select(t => t.TimeIn.Value).Single())
                 {
-                    return Ok("Vắng mặt");
+                    return "Vắng mặt";
                 }
                 // Trường hợp người dùng chưa check-in
-                return Ok("Chưa có mặt");
+                return "Chưa có mặt";
             }
             else
             {
@@ -48,19 +48,19 @@ namespace CarpentryWorkshopAPI.Controllers
                 {
                     if (DateTime.Now.TimeOfDay > team.Select(t => t.Timeout.Value).Single())
                     {
-                        return Ok("Vắng mặt");
+                        return "Vắng mặt";
                     }
-                    return Ok("Có mặt");
+                    return "Có mặt";
                 }else
                 {
-                    return Ok("Nhân viên đã check out");
+                    return "Nhân viên đã check out";
                 }
             }
 
         }
 
         [HttpGet("GetEmployeesByTeamLeaderId/{teamLeaderId}")]
-        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployeesByTeamLeaderId(int teamLeaderId)
+        public async Task<ActionResult<IEnumerable<CheckInOutListDTO>>> GetEmployeesByTeamLeaderId(int teamLeaderId)
         {
             var teamId = await _context.Teams
                 .Where(t => t.TeamLeaderId == teamLeaderId)
@@ -74,10 +74,16 @@ namespace CarpentryWorkshopAPI.Controllers
 
             var employees = await _context.EmployeeTeams
                 .Where(et => et.TeamId == teamId && et.EndDate == null)
-                .Select(et => et.Employee)
+                .Include(t => t.TeamId)
+                .Select(em => new CheckInOutListDTO
+                {
+                    EmployeeName = em.Employee.FirstName +" "+ em.Employee.LastName,
+                    EmployeeId = em.Employee.EmployeeId,
+                    EmployeeStatus = GetAttendanceStatus(em.EmployeeId)
+                })
                 .ToListAsync();
 
-            if (employees.Count == 0)
+            if (employees.Count() == 0)
             {
                 return NotFound("No employees found in the team with EndDate == null");
             }
