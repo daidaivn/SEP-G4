b383c76444561f540aab565f6df89649e3ba2005
HoangTran21012001 <hoangtran2k1000@gmail.com>
2023-10-27 12:02:36 +0700

    fix status

--- Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Controllers/CheckInOutController.cs
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Controllers/CheckInOutController.cs
@@ -2,6 +2,7 @@
 using CarpentryWorkshopAPI.DTO;
 using CarpentryWorkshopAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarpentryWorkshopAPI.Controllers
 {
@@ -16,6 +17,44 @@ namespace CarpentryWorkshopAPI.Controllers
             _context = context;
             _mapper = mapper;
         }
+        [HttpGet]
+        [Route("api/attendance/{employeeId}")]
+        public IActionResult GetAttendanceStatus(int employeeId)
+        {
+            DateTime currentDate = DateTime.Now.Date;
+
+            var lastAttendance = _context.CheckInOuts
+                .Where(a => a.EmployeeId == employeeId && a.Date == currentDate)
+                .OrderByDescending(a => a.TimeCheckIn)
+                .FirstOrDefault();
+            var team = _context.EmployeeTeams.Where(et => et.EmployeeId == employeeId && et.EndDate == null)
+                .Include(et => et.Team).ThenInclude(et => et.WorkSchedules).ThenInclude(et => et.ShiftType).Select(et => new
+                {
+                    TimeIn = et.Team.WorkSchedules.Select(ws=>ws.ShiftType.StartTime).Single(),
+                    Timeout = et.Team.WorkSchedules.Select(ws =>ws.ShiftType.EndTime).Single(),
+                });
+            if (lastAttendance == null)
+            {
+                if (DateTime.Now.TimeOfDay > team.Select(t => t.TimeIn.Value).Single())
+                {
+                    return Ok("Vắng mặt");
+                }
+                // Trường hợp người dùng chưa check-in
+                return Ok("Chưa có mặt");
+            }
+            else
+            {
+                if(lastAttendance.TimeCheckOut == null)
+                {
+                    return Ok("Có mặt");
+                }else
+                {
+                    return Ok("Nhân viên đã check out");
+                }
+            }
+
+        }
+
         [HttpGet("{employeeId}/{date}")]
         public IActionResult GetCheckInOutForEmoployeeId(int employeeId, string date)
         {
@@ -25,13 +64,13 @@ namespace CarpentryWorkshopAPI.Controllers
                                        System.Globalization.CultureInfo.InvariantCulture);
                 var checkInOut = _context.CheckInOuts.Where(a => a.Date.Value.Date == datetime && a.EmployeeId == employeeId).AsQueryable();
                 var checkIn = checkInOut.OrderBy(a => a.TimeCheckIn).FirstOrDefault();
-                var checkOut = checkInOut.OrderBy(a => a.TimeCheckOut).LastOrDefault();
+                var checkOut = checkInOut.OrderBy(a => a.TimeCheckIn).LastOrDefault();
                 if(checkInOut != null)
                 {
                     return Ok(new
                     {
                         checkIn = checkIn != null ? checkIn.TimeCheckIn.ToString() : "not yet",
-                        checkOut =checkOut != null ? checkOut.TimeCheckOut.ToString() : "not yet",
+                        checkOut =checkOut.TimeCheckOut != null ? checkOut.TimeCheckOut.ToString() : "not yet",
                     });
                 }
                 else
@@ -93,6 +132,8 @@ namespace CarpentryWorkshopAPI.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+
         [HttpPost]
         public IActionResult UpdateCheckInOutForEmployee([FromBody] CheckInOutDTO checkInOutDTO)
         {
@@ -142,6 +183,31 @@ namespace CarpentryWorkshopAPI.Controllers
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+        public void AutoCheckinIfForgotCheckout(int employeeId)
+        {
+            DateTime currentDate = DateTime.Now.Date;
+            DateTime previousDate = currentDate.AddDays(-1);
+
+
+            var lastAttendance = _context.CheckInOuts
+                .Where(a => a.EmployeeId == employeeId && a.Date == previousDate)
+                .OrderByDescending(a => a.TimeCheckIn)
+                .FirstOrDefault();
+
+            if (lastAttendance != null && lastAttendance.TimeCheckOut == null)
+            {
+                // Tự động thêm "check-in" cho ngày hôm nay
+                var autoCheckin = new CheckInOut
+                {
+                    EmployeeId = employeeId,
+                    TimeCheckIn = currentDate.TimeOfDay,
+                    Date = currentDate
+                };
+
+                _context.CheckInOuts.Add(autoCheckin);
+                _context.SaveChanges();
             }
         }
     }
