3a2c8458a9e84b1a11861181603e95a9727a2a54
unknown <donghiem114@gmail.com>
2023-10-28 12:16:57 +0700

    team

--- Code/CarpentryWorkshopClient/src/sevices/TeamService.js
+++ Code/CarpentryWorkshopClient/src/sevices/TeamService.js
@@ -7,4 +7,7 @@ const createTeam = (teamName) => {
   const name = teamName ; 
   return axios.post(`/CCMSapi/Team/AddTeam?name=${name}`);
 };
-export { fetchAllTeam, createTeam };
+const detailTeamByID = (teamID) => {
+  return axios.get(`/CCMSapi/Team/GetTeamMembers/${teamID}/members`);
+};
+export { fetchAllTeam, createTeam, detailTeamByID };
