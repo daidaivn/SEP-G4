9e1ea5c7b57be8f2bc55c8cc4d7e602331bdef5b
unknown <donghiem114@gmail.com>
2023-10-30 00:43:28 +0700

    Fe call APi team

--- Code/CarpentryWorkshopClient/src/sevices/Login copy.js
+++
@@ -1,9 +0,0 @@
-import axios from "./customize-axios"; // Import Axios instance
-
-const apiLogin = {
-  login: (userData) => {
-    return axios.post("/CCMSapi/Accounts/GetToken/gettoken", userData);
-  },
-};
-
-export default apiLogin;
--- Code/CarpentryWorkshopClient/src/sevices/PageService copy.js
+++
@@ -1,6 +0,0 @@
-import axios from "./customize-axios";
-
-const fetchAllPages = () => {
-  return axios.get("/CCMSapi/Pages/GetAllPage");
-};
-export { fetchAllPages };
