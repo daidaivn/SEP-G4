cb47ce21b2b7d68028b6288b2159894b2de40dac
HoangTran21012001 <hoangtran2k1000@gmail.com>
2023-11-05 10:24:11 +0700

    Fix models

--- Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Models/Dependent.cs
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Models/Dependent.cs
@@ -15,17 +15,17 @@ namespace CarpentryWorkshopAPI.Models
         public string? FullName { get; set; }
         public string? IdentifierCode { get; set; }
         public bool? Gender { get; set; }
-        public string? Relationship { get; set; }
         public DateTime? Dob { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
         public string? IdentifierName { get; set; }
         public bool? Status { get; set; }
         public string? NoteReason { get; set; }
-        public int? TypeDependentId { get; set; }
+        public string? Relation { get; set; }
+        public int? RelationshipId { get; set; }
 
         public virtual Employee? Employee { get; set; }
-        public virtual TypeDependent? TypeDependent { get; set; }
+        public virtual RelationshipsType? Relationship { get; set; }
         public virtual ICollection<DependentsStatusHistory> DependentsStatusHistories { get; set; }
     }
 }
