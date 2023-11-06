3625c6c94b19c5803a01482696a9b43547285186
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-11-06 21:05:50 +0700

    Fix code

---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Models/RelationshipType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarpentryWorkshopAPI.Models
+{
+    public partial class RelationshipType
+    {
+        public RelationshipType()
+        {
+            Dependents = new HashSet<Dependent>();
+        }
+
+        public int RelationshipId { get; set; }
+        public string? RelationshipName { get; set; }
+        public string? Note { get; set; }
+
+        public virtual ICollection<Dependent> Dependents { get; set; }
+    }
+}
