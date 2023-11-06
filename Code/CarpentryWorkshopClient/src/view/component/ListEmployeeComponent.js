55ca65da2e4abd6ca76858b231e04830a701f6ae
duong <handuong707@gmail.com>
2023-11-06 16:28:34 +0700

    popup add contract

--- Code/CarpentryWorkshopClient/src/view/component/ListEmployeeComponent.js
+++ Code/CarpentryWorkshopClient/src/view/component/ListEmployeeComponent.js
@@ -253,6 +253,17 @@ function ListEmployeeComponent() {
     setIsModalOpenAddRole(false);
   };
 
+  const [isModalOpenEditContract, setIsModalOpenEditContract] = useState(false);
+  const showModalEditContract = () => {
+    setIsModalOpenEditContract(true);
+  };
+  const handleOkEditContract = () => {
+    setIsModalOpenEditContract(false);
+  };
+  const handleCancelEditContract = () => {
+    setIsModalOpenEditContract(false);
+  };
+
   const [isModalOpenAddContract, setIsModalOpenAddContract] = useState(false);
   const showModalAddContract = () => {
     setIsModalOpenAddContract(true);
@@ -930,6 +941,111 @@ function ListEmployeeComponent() {
               {" "}
               <h3>Thêm hợp đồng</h3>
             </div>
+            <div className="body-add-role-employee body-modal-add-contract">
+              <table>
+                <thead className="thead-first">
+                  Bạn cần tạo hợp đồng để hoàn tất thêm nhân viên
+                </thead>
+                <div className="body-table body-table-contract">
+                  <tr>
+                    <Input
+                      className="select-input"
+                      placeholder="Mã hợp đồng"
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                      }}
+                    />
+                    <Select
+                      className="select-input"
+                      defaultValue="lucy"
+                      style={{
+                        width: "100%",
+                      }}
+                      onChange={handleChange}
+                      options={[
+                        {
+                          value: "jack",
+                          label: "Jack",
+                        },
+                        {
+                          value: "lucy",
+                          label: "Lucy",
+                        },
+                        {
+                          value: "Yiminghe",
+                          label: "yiminghe",
+                        },
+                        {
+                          value: "disabled",
+                          label: "Disabled",
+                        },
+                      ]}
+                    />
+                  </tr>
+
+                  <tr>
+                    <div className="input-date">
+                      <Input
+                        className="select-input"
+                        placeholder="Thời gian bắt đầu"
+                        type="date"
+                        style={{
+                          width: "100%",
+                        }}
+                      />
+                    </div>
+                    <div className="input-date">
+                      <Input
+                        className="select-input"
+                        placeholder="Thời gian bắt đầu"
+                        type="date"
+                        style={{
+                          width: "100%",
+                        }}
+                      />
+                    </div>
+                  </tr>
+                  <tr>
+                    <div className="input-date">
+                      <Input
+                        className="select-input"
+                        placeholder="Đường dẫn hợp đồng"
+                        style={{
+                          width: "100%",
+                        }}
+                      />
+                    </div>
+                  </tr>
+                </div>
+                <thead className="thead-last"></thead>
+              </table>
+            </div>
+            <div className="modal-footer modal-footer-add-employee add">
+              <button className="btn-cancel" onClick={handleCancelAddContract}>
+                Hủy bỏ
+              </button>
+              <button
+                className="btn-edit btn-save"
+                onClick={handleOkAddContract}
+              >
+                Lưu
+              </button>
+            </div>
+          </div>
+        </Modal>
+        <Modal
+          className="modal"
+          open={isModalOpenEditContract}
+          on
+          Ok={handleOkEditContract}
+          onCancel={handleCancelEditContract}
+        >
+          <div className="modal-add-roleyee-employee modal-contract">
+            <div className="modal-head">
+              {" "}
+              <h3>Sửa hợp đồng</h3>
+            </div>
             <div className="body-add-role-employee">
               <table>
                 <thead className="thead-first"></thead>
@@ -1016,12 +1132,12 @@ function ListEmployeeComponent() {
               </table>
             </div>
             <div className="modal-footer modal-footer-add-employee add">
-              <button className="btn-cancel" onClick={handleCancelAddContract}>
+              <button className="btn-cancel" onClick={handleCancelEditContract}>
                 Hủy bỏ
               </button>
               <button
                 className="btn-edit btn-save"
-                onClick={handleOkAddContract}
+                onClick={handleOkEditContract}
               >
                 Lưu
               </button>
@@ -1469,7 +1585,7 @@ function ListEmployeeComponent() {
               <div className="btn-left">
                 <div
                   className="modal-footer1 add-origin"
-                  // onClick={showModalContract}
+                  onClick={showModalEditContract}
                 >
                   Sửa hợp đồng
                 </div>
