9493f26457fdac0e984a402344a56fa875ff64c1
HoangTran21012001 <hoangtran2k1000@gmail.com>
2023-11-06 23:40:01 +0700

    Fix

--- Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/DTO/SalaryListDTO.cs
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/DTO/SalaryListDTO.cs
@@ -3,13 +3,14 @@
     public class SalaryListDTO
     {
         public int SalaryId { get; set; }
-        public string? SalaryName { get; set; }
+        public double? SalaryName { get; set; }
         public int? SalaryDetailId { get; set; }
         public double? AmountOfMoney { get; set; }
-        public int? EmloyeeIdInput { get; set; }
+        public int? EmloyeeId { get; set; }
         public string EmployeeName { get; set; }
-        public int? MonthSalary { get; set; }
-        public int? Year { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
         
     }
 }
