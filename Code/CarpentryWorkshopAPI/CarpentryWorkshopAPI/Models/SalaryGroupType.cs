428342aa5bea1b1b828dff9e0b919a96d3e4faca
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-11-08 19:40:15 +0700

    Fix code for team

---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Models/SalaryGroupType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarpentryWorkshopAPI.Models
+{
+    public partial class SalaryGroupType
+    {
+        public SalaryGroupType()
+        {
+            SalaryTypes = new HashSet<SalaryType>();
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public virtual ICollection<SalaryType> SalaryTypes { get; set; }
+    }
+}
