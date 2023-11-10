9dd2f6049d660b4040665c18ab872479f30b5167
duong <handuong707@gmail.com>
2023-11-10 12:35:55 +0700

    popup view salary user

--- Code/CarpentryWorkshopClient/src/view/component/componentUI/ListUserHeader.js
+++ Code/CarpentryWorkshopClient/src/view/component/componentUI/ListUserHeader.js
@@ -257,40 +257,151 @@ function ListUserHeader() {
       >
         <div className="modal-payroll">
           <div className="modal-head">
-            {" "}
-            <h3>Bảng lương</h3>
+            <div className="body-payroll1">
+              <p>Bảng lương</p>
+              <Select
+                className="select-input"
+                defaultValue="lucy"
+                style={{
+                  width: "236",
+                }}
+                onChange={handleChange}
+                options={[
+                  {
+                    value: "jack",
+                    label: "Jack",
+                  },
+                  {
+                    value: "lucy",
+                    label: "Lucy",
+                  },
+                  {
+                    value: "Yiminghe",
+                    label: "yiminghe",
+                  },
+                  {
+                    value: "disabled",
+                    label: "Disabled",
+                  },
+                ]}
+              />
+            </div>
+            <div className="close">
+              <svg
+                onClick={handleCancelPayroll}
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="none"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M30 2.30769L17.3077 15L30 27.6923L27.6923 30L15 17.3077L2.30769 30L0 27.6923L12.6923 15L0 2.30769L2.30769 0L15 12.6923L27.6923 0L30 2.30769Z"
+                  fill="white"
+                />
+              </svg>
+            </div>
           </div>
-          <div className="body-payroll1">
-            <p>Lương tháng:</p>
-            <Select
-              className="select-input"
-              defaultValue="lucy"
-              style={{
-                width: "236",
-              }}
-              onChange={handleChange}
-              options={[
-                {
-                  value: "jack",
-                  label: "Jack",
-                },
-                {
-                  value: "lucy",
-                  label: "Lucy",
-                },
-                {
-                  value: "Yiminghe",
-                  label: "yiminghe",
-                },
-                {
-                  value: "disabled",
-                  label: "Disabled",
-                },
-              ]}
-            />
-          </div>
-          <div className="body-payroll2">
-            <div className="body-child"></div>
+          <div className="body-payroll">
+            <div className="body-child1 scrollbar" id="style-15">
+              <div className="body-child1-cn">
+                <div className="item-chil1">
+                  <div className="item-chil1-cn">
+                    <p className="text1">1.</p>
+                    <p>Lương chính</p>
+                    <div className="money">
+                      <p>9.000.000 VND</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="item-child1-col">
+                <div className="child1-col">
+                  <div className="child1-text">
+                    <p className="text1">2.</p>
+                    <p>Phụ cấp</p>
+                    <div className="money"></div>
+                  </div>
+                </div>
+                <div className="child1-col">
+                  <div className="child1-text">
+                    <p className="text1">-</p>
+                    <p>Trách nhiệm</p>
+                    <div className="money">
+                      <p>9.000.000 VND</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="child1-col">
+                  <div className="child1-text">
+                    <p className="text1">-</p>
+                    <p>Ăn trưa</p>
+                    <div className="money">
+                      <p>9.000.000 VND</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="child1-col">
+                  <div className="child1-text">
+                    <p className="text1">-</p>
+                    <p>Điện thoại</p>
+                    <div className="money">
+                      <p>9.000.000 VND</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="child1-col">
+                  <div className="child1-text">
+                    <p className="text1">-</p>
+                    <p>Xăng xe</p>
+                    <div className="money">
+                      <p>9.000.000 VND</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="item-child1-col">
+                <div className="child1-col">
+                  <div className="child1-text">
+                    <p className="text1">3.</p>
+                    <p>Thưởng</p>
+                    <div className="money"></div>
+                  </div>
+                </div>
+                <div className="child1-col">
+                  <div className="child1-text">
+                    <p className="text1">-</p>
+                    <p>Tết dương lịch</p>
+                    <div className="money">
+                      <p>9.000.000 VND</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="child1-col">
+                  <div className="child1-text">
+                    <p className="text1">-</p>
+                    <p>Tết âm lịch</p>
+                    <div className="money">
+                      <p>9.000.000 VND</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="body-child2">
+              <div className="body-child1-cn">
+                <div className="item-chil1">
+                  <div className="item-chil1-cn">
+                    <p>Tổng số tiền nhận được</p>
+                    <div className="money">
+                      <p>19.000.000 VND</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Modal>
