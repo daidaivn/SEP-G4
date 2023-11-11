2765dbc1cb9e6019e7afe1ed52d2294c25048625
HoangTran21012001 <hoangtran2k1000@gmail.com>
2023-11-11 12:50:32 +0700

    Fix

--- Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/DTO/EmployeeDetailBasicDTO.cs
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/DTO/EmployeeDetailBasicDTO.cs
@@ -10,7 +10,7 @@
         public string? Genderstring { get; set; }
         public string? Address { get; set; }
         public string? PhoneNumber { get; set; }
-        public int? TaxId { get; set; }
+        public string? TaxId { get; set; }
         public string? Email { get; set; }
         public string? Country { get; set; }
         public int? CountryId { get; set; }
