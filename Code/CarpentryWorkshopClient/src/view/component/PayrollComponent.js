cd4988639cd97f7a5a5dc37c3398651bd40fc692
duong <handuong707@gmail.com>
2023-11-12 13:25:41 +0700

    code popup Reward Company

--- Code/CarpentryWorkshopClient/src/view/component/PayrollComponent.js
+++ Code/CarpentryWorkshopClient/src/view/component/PayrollComponent.js
@@ -7,6 +7,10 @@ import ListUserHeader from "./componentUI/ListUserHeader";
 import { Form, Input, Select, Switch, Modal } from "antd";
 import { useState } from "react";
 const PayrollComponent = () => {
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+
   //modal Chi tiết phụ cấp
   const [isModalOpenSubsidies, setIsModalOpenSubsidies] = useState(false);
   const showModalSubsidies = () => {
@@ -41,6 +45,20 @@ const PayrollComponent = () => {
   };
   const handleCancelReward = () => {
     setIsModalOpenReward(false);
+  };
+
+  //modal Thưởng công ty
+  const [isModalOpenRewardCompany, setIsModalOpenRewardCompany] = useState(
+    false
+  );
+  const showModalRewardCompany = () => {
+    setIsModalOpenRewardCompany(true);
+  };
+  const handleOkRewardCompany = () => {
+    setIsModalOpenRewardCompany(false);
+  };
+  const handleCancelRewardCompany = () => {
+    setIsModalOpenRewardCompany(false);
   };
 
   return (
@@ -173,6 +191,8 @@ const PayrollComponent = () => {
           </div>
           <div className="ListWork">
             <svg
+              //show Modal Thưởng công ty
+              onClick={showModalRewardCompany}
               xmlns="http://www.w3.org/2000/svg"
               width="36"
               height="37"
@@ -213,64 +233,6 @@ const PayrollComponent = () => {
               />
             </svg>
           </div>
-
-          <i className="icon-responsive icon-filter">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-            >
-              <path
-                d="M7.15979 9.53513C7.15979 9.84013 6.95977 10.2401 6.70477 10.3951L5.99979 10.8501C5.34479 11.2551 4.43478 10.8001 4.43478 9.99013V7.31512C4.43478 6.96012 4.23479 6.50513 4.02979 6.25513L2.10977 4.23512C1.85477 3.98012 1.65479 3.53013 1.65479 3.22513V2.06512C1.65479 1.46012 2.10979 1.00513 2.66479 1.00513H9.33478C9.88978 1.00513 10.3448 1.46012 10.3448 2.01512V3.12512C10.3448 3.53012 10.0898 4.03513 9.83978 4.28513"
-                stroke="white"
-                stroke-miterlimit="10"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8.03506 8.26006C8.91872 8.26006 9.63507 7.54372 9.63507 6.66006C9.63507 5.77641 8.91872 5.06006 8.03506 5.06006C7.15141 5.06006 6.43506 5.77641 6.43506 6.66006C6.43506 7.54372 7.15141 8.26006 8.03506 8.26006Z"
-                stroke="white"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M9.93506 8.56006L9.43506 8.06006"
-                stroke="white"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </i>
-          <i className="icon-responsive icon-filter icon-add">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="10"
-              height="10"
-              viewBox="0 0 10 10"
-              fill="none"
-            >
-              <path
-                d="M8.48719 6.94879H7.56411V6.02571C7.56411 5.8575 7.42462 5.71802 7.25642 5.71802C7.08821 5.71802 6.94873 5.8575 6.94873 6.02571V6.94879H6.02565C5.85744 6.94879 5.71796 7.08827 5.71796 7.25648C5.71796 7.42468 5.85744 7.56417 6.02565 7.56417H6.94873V8.48725C6.94873 8.65545 7.08821 8.79494 7.25642 8.79494C7.42462 8.79494 7.56411 8.65545 7.56411 8.48725V7.56417H8.48719C8.65539 7.56417 8.79488 7.42468 8.79488 7.25648C8.79488 7.08827 8.65539 6.94879 8.48719 6.94879Z"
-                fill="white"
-              />
-              <path
-                opacity="0.4"
-                d="M9.10254 3.57233V1.70977C9.10254 1.13131 8.83997 0.897461 8.18766 0.897461H6.53023C5.87792 0.897461 5.61536 1.13131 5.61536 1.70977V3.56823C5.61536 4.15079 5.87792 4.38054 6.53023 4.38054H8.18766C8.83997 4.38464 9.10254 4.15079 9.10254 3.57233Z"
-                fill="white"
-              />
-              <path
-                d="M4.38458 3.57233V1.70977C4.38458 1.13131 4.12202 0.897461 3.46971 0.897461H1.81227C1.15996 0.897461 0.8974 1.13131 0.8974 1.70977V3.56823C0.8974 4.15079 1.15996 4.38054 1.81227 4.38054H3.46971C4.12202 4.38464 4.38458 4.15079 4.38458 3.57233Z"
-                fill="white"
-              />
-              <path
-                opacity="0.4"
-                d="M4.38458 8.18779V6.53035C4.38458 5.87804 4.12202 5.61548 3.46971 5.61548H1.81227C1.15996 5.61548 0.8974 5.87804 0.8974 6.53035V8.18779C0.8974 8.84009 1.15996 9.10266 1.81227 9.10266H3.46971C4.12202 9.10266 4.38458 8.84009 4.38458 8.18779Z"
-                fill="white"
-              />
-            </svg>
-          </i>
         </div>
         <table className="list-table ">
           <thead>
@@ -1212,6 +1174,126 @@ const PayrollComponent = () => {
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </Modal>
+
+        {/* Modal Thưởng công ty */}
+        <Modal
+          className="modal-reward-company-all"
+          open={isModalOpenRewardCompany}
+          on
+          Ok={handleOkRewardCompany}
+          onCancel={handleCancelRewardCompany}
+        >
+          <div className="modal-payroll modal-reward-company">
+            <div className="modal-head">
+              <div className="body-payroll1">
+                <p>Thưởng công ty</p>
+                <Select
+                  className="select-input"
+                  defaultValue="lucy"
+                  style={{
+                    width: "236",
+                  }}
+                  onChange={handleChange}
+                  options={[
+                    {
+                      value: "jack",
+                      label: "Jack",
+                    },
+                    {
+                      value: "lucy",
+                      label: "Lucy",
+                    },
+                    {
+                      value: "Yiminghe",
+                      label: "yiminghe",
+                    },
+                    {
+                      value: "disabled",
+                      label: "Disabled",
+                    },
+                  ]}
+                />
+              </div>
+              <div className="close">
+                <svg
+                  onClick={handleCancelRewardCompany}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 30 30"
+                  fill="none"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M30 2.30769L17.3077 15L30 27.6923L27.6923 30L15 17.3077L2.30769 30L0 27.6923L12.6923 15L0 2.30769L2.30769 0L15 12.6923L27.6923 0L30 2.30769Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="body-reward">
+              <div className="div1">
+                <thead>
+                  <td className="first">STT</td>
+                  <td className="middle">Loại thưởng</td>
+                  <td className="middle">Số tiền thưởng / người</td>
+                  <td className="middle">Người được thưởng</td>
+                  <td className="last">Ngày khen thưởng</td>
+                </thead>
+                <div className="item-reward">
+                  <p className="stt">1</p>
+                  <div className="child-reward">
+                    <p>Thưởng Tết</p>
+                  </div>
+                  <div className="child-reward">
+                    <p>1.000.000</p>
+                  </div>
+                  <div className="child-reward">
+                    <p>Toàn thể nhân viên</p>
+                  </div>
+                  <div className="child-reward last1">
+                    <p>20/10/2023</p>
+                  </div>
+                </div>
+                <div className="item-reward">
+                  <p className="stt">2</p>
+                  <div className="child-reward">
+                    <p>Nhân viên xuất sắc</p>
+                  </div>
+                  <div className="child-reward">
+                    <p>500.000</p>
+                  </div>
+                  <div className="child-reward">
+                    <p>Nguyễn Văn an</p>
+                  </div>
+                  <div className="child-reward last1">
+                    <p>30/10/2023</p>
+                  </div>
+                </div>
+                <div className="item-reward">
+                  <p className="stt">3</p>
+                  <div className="child-reward">
+                    <p>Thưởng 30/4 - 1/5</p>
+                  </div>
+                  <div className="child-reward">
+                    <p>500.000</p>
+                  </div>
+                  <div className="child-reward">
+                    <p>Toàn thể nhân viên</p>
+                  </div>
+                  <div className="child-reward last1">
+                    <p>15/11/2023</p>
+                  </div>
+                </div>
+              </div>
+              <div className="div2">
+                <span className="btn-reward">Thưởng cá nhân</span>
+                <span className="btn-reward">Thưởng toàn thể công ty</span>
               </div>
             </div>
           </div>
