62a12bb8ef45a0a6b70832657f9cb862d5904f32
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-11-12 22:56:55 +0700

    Update code employee

---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Controllers/WorkShiftController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using CarpentryWorkshopAPI.DTO;
+using CarpentryWorkshopAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.Xml;
+
+namespace CarpentryWorkshopAPI.Controllers
+{
+    [ApiController]
+    [Route("CCMSapi/[controller]/[action]")]
+    public class WorkShiftController : Controller
+    {
+        private readonly SEPG4CCMSContext _context;
+        private readonly IMapper _mapper;
+        public WorkShiftController(SEPG4CCMSContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        //[HttpGet]
+        //public IActionResult GetAllWorkShift()
+        //{
+        //    try
+        //    {
+        //        var wst = _context.WorkSchedules
+        //            .Include(x => x.ShiftType)
+        //            .Include(x => x.Team)
+        //            .ThenInclude(t => t.TeamWorks)
+        //            .ThenInclude(tw => tw.Work)
+        //            .Select(ws => new WorkShiftDTO
+        //            {
+        //                ShiftTypeId = ws.ShiftTypeId,
+        //                TypeName = ws.ShiftType.TypeName,
+        //                WorkId = ws.Team.TeamWorks.Select(x => x.WorkId).FirstOrDefault(),
+        //                WorkName = ws.Team.TeamWorks.Select(tw => tw.Work.WorkName).FirstOrDefault(),
+        //                WorkTime = ws.ShiftType.StartTime + " - " + ws.ShiftType.EndTime,
+        //                Date = ws.Team.TeamWorks.Select(tw => tw.Date).FirstOrDefault().Value.Date,
+
+        //            });
+
+
+        //    }catch(Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+    }
+}
---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/DTO/WorkShiftDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CarpentryWorkshopAPI.DTO
+{
+    public class WorkShiftDTO
+    {
+        public int? ShiftTypeId { get; set; }
+        public string? TypeName { get; set; }
+        public int? WorkId { get; set; }
+        public string? WorkName { get; set; }
+        public string? WorkTime { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Status { get; set; }
+    }
+}
