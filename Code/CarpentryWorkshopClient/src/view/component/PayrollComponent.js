71f49860ed0322ef7e10cef90c1f1cee8c143031
duong <handuong707@gmail.com>
2023-11-12 13:36:41 +0700

    code popup Reward all company

--- Code/CarpentryWorkshopClient/src/view/component/PayrollComponent.js
+++ Code/CarpentryWorkshopClient/src/view/component/PayrollComponent.js
@@ -1411,6 +1411,57 @@ const PayrollComponent = () => {
           </div>
         </Modal>
         {/* Modal Thưởng toàn thể công ty */}
+        <Modal
+          className="modal"
+          open={isModalOpenRewardAll}
+          onOk={handleOkRewardAll}
+          onCancel={handleCancelRewardAll}
+        >
+          <div className="modal-detail-all">
+            <div className="head-modal">
+              <p>Thưởng toàn thể công ty</p>
+            </div>
+            <div className="body-modal">
+              <div className="item-modal">
+                <p>Loại thưởng</p>
+                <Select
+                  defaultValue="lucy"
+                  style={{
+                    width: 120,
+                  }}
+                  onChange={handleChange}
+                  options={[
+                    {
+                      value: "jack",
+                      label: "Jack",
+                    },
+                    {
+                      value: "lucy",
+                      label: "Lucy",
+                    },
+                    {
+                      value: "Yiminghe",
+                      label: "yiminghe",
+                    },
+                  ]}
+                />
+              </div>
+              <div className="item-modal">
+                <p>Số tiền thưởng:</p>
+                <Input type="text"></Input>
+              </div>
+
+              <div className="footer-modal">
+                <span className="back" onClick={handleOkRewardAll}>
+                  Hủy bỏ
+                </span>
+                <span className="edit save" onClick={handleCancelRewardAll}>
+                  Lưu
+                </span>
+              </div>
+            </div>
+          </div>
+        </Modal>
       </div>
     </>
   );
