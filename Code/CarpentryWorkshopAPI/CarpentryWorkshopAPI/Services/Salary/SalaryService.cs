eb2a0bea76377321d3d4ce3615338c4b755f27a0
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-11-18 13:34:22 +0700

    Add controller

---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Services/Salary/SalaryService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CarpentryWorkshopAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarpentryWorkshopAPI.Services.Salary
+{
+    public class SalaryService
+    {
+        private readonly SEPG4CCMSContext _context;
+        private readonly IMapper _mapper;
+        public SalaryService(SEPG4CCMSContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public dynamic GetAllSalarys()
+        {
+            var allsalarys = _context.Employees
+                .Include(x => x.Salaries)
+                .Include(x => x.BonusDetails)
+                .Include(x => x.SpecialOccasions)
+                .Include(x => x.Subsidies)
+                .Include(x => x.EmployeesAllowances)
+                .ThenInclude(ea => ea.AllowanceType)
+                .ThenInclude(at => at.Allowance)
+                .Select(s => new
+                {
+                    MainSalary = s.Salaries
+                        .OrderByDescending(sa => sa.PayDate)
+                        .Where(sa => sa.EmployeeId == s.EmployeeId)
+                        .Select(sa => sa.TotalSalary)
+                        .FirstOrDefault(),
+                    Subsidys = s.SpecialOccasions
+                        .OrderByDescending(so => so.OccasionDate)
+                        .Where(so => so.EmployeeId == s.EmployeeId)
+                        .Select(so => so.Amount)
+                        .FirstOrDefault(),
+                    Bonus = s.BonusDetails
+                        .OrderByDescending(bd => bd.BonusDate)
+                        .Where(bd => bd.EmployeeId == s.EmployeeId)
+                        .Select(bd => bd.BonusAmount)
+                        .FirstOrDefault(),
+
+                });
+            return "";
+        }
+    }
+}
