bdc1f52c66aa759fc684561c9e6b1dfc22c5fa2d
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-11-20 01:04:08 +0700

    Update

--- Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Models/Allowance.cs
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Models/Allowance.cs
@@ -12,6 +12,7 @@ namespace CarpentryWorkshopAPI.Models
 
         public int AllowanceId { get; set; }
         public string? Name { get; set; }
+        public bool? Status { get; set; }
 
         public virtual ICollection<AllowanceType> AllowanceTypes { get; set; }
     }
