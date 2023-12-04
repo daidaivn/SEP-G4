3c585d370d8d88b241c3362a3d9d5b468aaaa650
unknown <donghiem114@gmail.com>
2023-12-04 21:32:10 +0700

    fix

--- Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Controllers/ExcelSalaryController.cs
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Controllers/ExcelSalaryController.cs
@@ -16,24 +16,20 @@ namespace CarpentryWorkshopAPI.Controllers
             _excelSalarySevice = excelSalarySevice;
         }
 
-
+       
 
         [HttpGet("ContractsByDate/{month}/{year}")]
-        public async Task<ActionResult<IEnumerable<EmployeeInfo>>> GetEmployeesByContractDate(int month, int year)
+        public async Task<ActionResult<IEnumerable<EmployeeInfo>>> GetEmployeesByContractDate( int month, int year)
         {
             var employees = await _excelSalarySevice.GetEmployeesByContractDateAsync(month, year);
             return Ok(employees);
         }
 
-        [HttpPost("export/{month}/{year}")]
-        public async Task<IActionResult> ExportSalaryExcel(int month, int year)
+        [HttpPost("generate-excel/{month}/{year}")]
+        public async Task<IActionResult> GenerateExcel(int month, int year)
         {
-            var stream = await _excelSalarySevice.GenerateExcelAsync(month, year);
-            string fileName = $"SalaryReport_{month}_{year}.xlsx";
-            string fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-            return File(stream, fileType, fileName);
+            var processedFile = await _excelSalarySevice.GenerateExcelAsync(month, year);
+            return File(processedFile.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "generatedFile.xlsx");
         }
-
     }
 }
-
