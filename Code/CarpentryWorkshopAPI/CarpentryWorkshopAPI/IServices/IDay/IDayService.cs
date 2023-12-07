7fe6c9c877ee85961356767759f515027caebc9f
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-12-07 17:38:17 +0700

    Fix

---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/IServices/IDay/IDayService.cs
@@ -0,0 +1,9 @@
+﻿using CarpentryWorkshopAPI.Models;
+
+namespace CarpentryWorkshopAPI.IServices.IDay
+{
+    public interface IDayService
+    {
+        int CalculateNumberOfHolidays(IEnumerable<HolidaysDetail> holidayGroup);
+    }
+}
---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Services/Day/DayService.cs
@@ -0,0 +1,25 @@
+﻿using CarpentryWorkshopAPI.IServices.IDay;
+using CarpentryWorkshopAPI.Models;
+
+namespace CarpentryWorkshopAPI.Services.Day
+{
+    public class DayService : IDayService
+    {
+        public int CalculateNumberOfHolidays(IEnumerable<HolidaysDetail> holidayGroup)
+        {
+            var orderedDates = holidayGroup.OrderBy(x => x.Date).Select(x => x.Date.Value).ToList();
+
+            int numberOfDays = 0;
+
+            for (int i = 0; i < orderedDates.Count - 1; i++)
+            {
+                int daysBetween = (int)(orderedDates[i + 1] - orderedDates[i]).TotalDays;
+                numberOfDays += daysBetween;
+            }
+
+            numberOfDays += 1;
+
+            return numberOfDays;
+        }
+    }
+}
