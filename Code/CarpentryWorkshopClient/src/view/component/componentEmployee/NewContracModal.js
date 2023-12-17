8c95e6e730d61f0ae2f899e36c993f3f3f7e7c53
unknown <donghiem114@gmail.com>
2023-12-17 09:19:20 +0700

    add

--- Code/CarpentryWorkshopClient/src/view/component/componentEmployee/NewContracModal.js
+++ Code/CarpentryWorkshopClient/src/view/component/componentEmployee/NewContracModal.js
@@ -69,6 +69,7 @@ const NewContractModal = ({
                           }))
                         : []
                     }
+                    placeholder="Loại hợp đồng"
                   />
                 </div>
               </tr>
