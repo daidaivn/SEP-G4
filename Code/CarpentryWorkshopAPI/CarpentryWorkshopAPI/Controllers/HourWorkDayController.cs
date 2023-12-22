db39dd93a97f9bf56ae59cf0a1eb705a169d2a72
HoangTran21012001 <hoangtran2k1000@gmail.com>
2023-12-22 15:41:50 +0700

    ix

---
+++ Code/CarpentryWorkshopAPI/CarpentryWorkshopAPI/Controllers/HourWorkDayController.cs
@@ -0,0 +1,18 @@
+﻿using CarpentryWorkshopAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpentryWorkshopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HourWorkDayController : ControllerBase
+    {
+        private readonly SEPG4CWMSContext _context;
+
+        public HourWorkDayController(SEPG4CWMSContext context)
+        {
+            _context = context;
+        }
+    }
+}
